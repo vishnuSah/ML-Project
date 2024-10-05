cf9a05e6584edbde9c75cbd0669054a116e93bad
Vishnu <vishnusah449@gmail.com>
2024-10-05 23:37:29 +0530

    intergrated model with django code

--- machine_learning_project/placement_predictor/placement_predictor/views.py
+++ machine_learning_project/placement_predictor/placement_predictor/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import HttpResponse, render
+import joblib
 
+
+def read_model(arr):
+    model_logistic = joblib.load('placement_predictor/model/placement_predictor.joblib')
+    scaler = joblib.load('placement_predictor/model/scaler.joblib')
+
+    scaled_data = scaler.transform(arr)
+    result = model_logistic.predict(scaled_data)
+
+    return result
 
 def home(request):
     
     if request.method == 'POST':
         data = request.POST
-        iq = data['iq']
         cgpa = data['cgpa']
-        print(iq,cgpa)
+        iq = data['iq']
+        # print(iq,cgpa)
 
-        context = {
-            "output":"Placement ho jayega"
-        }
+        res = read_model([[cgpa,iq]])
+        print(res)
+        if res[0] == 1:
+            context = {
+                "output":"Placement ho jayega"
+            }
+        else:
+            context = {
+                "output":"Placement nahi hoga bhai"
+            }
+
 
         return render(request,'home.html', context=context)
     
 
     return render(request, 'home.html')
+
+
